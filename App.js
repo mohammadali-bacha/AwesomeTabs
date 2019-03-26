53ccfe25dc028362e5847d49ecb1f674622b43f2
m-ali <bacha@tuta.io>
2019-03-26 12:56:13 +0100

    Configuring the header bar

--- App.js
+++ App.js
@@ -1,8 +1,24 @@
 import React from 'react';
-import { Button, View, Text } from 'react-native';
-import { createStackNavigator, createAppContainer } from 'react-navigation';
+import { Button, Image, View, Text } from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation'; // Version can be specified in package.json
+
+class LogoTitle extends React.Component {
+  render() {
+    return (
+      <Image
+        source={require('./spiro.png')}
+        style={{ width: 30, height: 30 }}
+      />
+    );
+  }
+}
 
 class HomeScreen extends React.Component {
+  static navigationOptions = {
+    // headerTitle instead of title
+    headerTitle: <LogoTitle />,
+  };
+
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
@@ -23,10 +39,25 @@ class HomeScreen extends React.Component {
 }
 
 class DetailsScreen extends React.Component {
+  static navigationOptions = ({ navigation, navigationOptions }) => {
+    console.log(navigationOptions);
+    // Notice the logs ^
+    // sometimes we call with the default navigationOptions and other times
+    // we call this with the previous navigationOptions that were returned from
+    // this very function
+    return {
+      title: navigation.getParam('otherParam', 'A Nested Details Screen'),
+      headerStyle: {
+        backgroundColor: navigationOptions.headerTintColor,
+      },
+      headerTintColor: navigationOptions.headerStyle.backgroundColor,
+    };
+  };
+
   render() {
     /* 2. Get the param, provide a fallback value if not available */
     const { navigation } = this.props;
-    const itemId = navigation.getParam('itemId', 'NO-ID');
+    const itemId = navigation.getParam('itemId', 'NO-ID'); 
     const otherParam = navigation.getParam('otherParam', 'some default value');
 
     return (
@@ -40,6 +71,11 @@ class DetailsScreen extends React.Component {
             this.props.navigation.push('Details', {
               itemId: Math.floor(Math.random() * 100),
             })}
+        />
+        <Button
+          title="Update the title"
+          onPress={() =>
+            this.props.navigation.setParams({ otherParam: 'Updated!' })}
         />
         <Button
           title="Go to Home"
@@ -61,6 +97,16 @@ const RootStack = createStackNavigator(
   },
   {
     initialRouteName: 'Home',
+    /* The header config from HomeScreen is now here */
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: '#f4511e',
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        fontWeight: 'bold',
+      },
+    },
   }
 );
 
